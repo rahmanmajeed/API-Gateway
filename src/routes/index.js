0704f36faaa713394e0242f54ea96a0cfa977c74
Majeed <rahmanmajeed46@gmail.com>
2022-11-26 23:41:05 +0600

    test::dummy route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
+//dummy route...
+router.get('/dummy', async(req, res,next) =>{
+    res.status(200).json({
+        status:'app is working...'
+    })
+})
 module.exports = router
