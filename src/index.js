6b4efb7ce8fc508d8130786b828b5ba73cbecfa1
Majeed <rahmanmajeed46@gmail.com>
2022-11-28 23:20:25 +0600

    add::dummy route.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const router = require('./routes')
 
 
 const app = express();
 const PORT = 5000;
+
+app.use(router)
 
 const server = app.listen(PORT, () => console.log(`Server is running ${PORT} :port`));
 // Handling Error
