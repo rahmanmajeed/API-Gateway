20376c1a012d08e941d9bb48a7c261ed3aa84762
Majeed <rahmanmajeed46@gmail.com>
2022-12-04 02:49:50 +0600

    [add] env file.

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
-  await mongoose.connect(DB, {
+  await mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   console.log("MongoDB Connected");
 };
+module.exports = connectDB
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const router = require('./routes')
+const router = require('./routes');
+const dbconnect = require('./config/db')
 
 
 const app = express();
 const PORT = 5000;
-
+dbconnect()
 app.use(router)
 
 const server = app.listen(PORT, () => console.log(`Server is running ${PORT} :port`));
+console.log('--- API Gateway Service ---')
+console.log('Connecting to API repository...')
 // Handling Error
-process.on('unhandledRejection', (err) => {
-    console.log(`An error occurred: ${err.message}`);
-    server.close(() => process.exit(1));
-  });
-  
+process.on('uncaughtException', (err) => {
+  console.error('Unhandled Exception', err)
+})
+
+process.on('uncaughtRejection', (err, promise) => {
+  console.error('Unhandled Rejection', err)
+})
