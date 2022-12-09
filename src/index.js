4904130f1057be7de04d9a97b2d9c469b6960759
Majeed <rahmanmajeed46@gmail.com>
2022-12-09 23:38:40 +0600

    [add] external library.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = require('./routes');
-const dbconnect = require('./config/db')
-
+const dbconnect = require('./config/db');
+const debug = require('debug')('http');
+const morgan = require('morgan');
+const numCPUs = require('os').cpus().length;
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
 dbconnect()
 app.use(router)
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+//test output
 const server = app.listen(PORT, () => console.log(`Server is running ${PORT} :port`));
 console.log('--- API Gateway Service ---')
 console.log('Connecting to API repository...')
